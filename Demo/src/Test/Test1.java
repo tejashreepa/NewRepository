17e2b5912d5c6abeb1b203c63dfc8f7ab37ee801
tejashreepa <tejupatil8888@gmail.com>
2022-03-31 10:53:18 +0530

    Done with Test1

---
+++ Demo/src/Test/Test1.java
@@ -0,0 +1,11 @@
+package Test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b=10;
+		System.out.println(a*b);
+	}
+
+}
---
+++ Demo/src/Test/Test2.java
@@ -0,0 +1,17 @@
+package Test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		String test= "Welcome to Java";
+		
+		if(test.contains("Java"))
+		{
+			System.out.println("Found!!!");
+		}
+
+
+	}
+
+}
